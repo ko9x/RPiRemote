f097c31548dedd83ec88f867504a1a613644e95d
Sean McGillis <slcidevice@yahoo.com>
2020-04-28 21:23:47 -0600

    nested to functions to make sure the other relays are off before turning on any new ones. Also created a function to show when the connection has been made.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,28 +9,84 @@ import { HttpClient, HttpHeaders} from '@angular/common/http';
   templateUrl: 'home.html'
 })
 
+
 export class HomePage {
 
+  private connectionReady = false;
+
     constructor(public navCtrl: NavController, public http: HttpClient) {
+      this.checkConnection();
     }
 
-    pinOn24() {
-      let headers = new HttpHeaders();
-      headers.append('Content-Type', 'application/x-www-form-urlencoded');
-      headers.append('Accept', 'application/json');
-      headers.append('Authorization', 'Basic YWRtaW46RnJhZ2dsZVN0aWNrQ2Fy');
+    checkConnection() {
+      this.http.get("http://192.168.0.31/rpiapi")
+      .subscribe(response => {
+        if(response) {
+          this.connectionReady = true;
+        } 
+      });
+    }
 
-      this.http.get("http://192.168.0.31/rpiapi/activate/24", {headers:headers})
+    //  Lights section
+    pinOn7() {
+      this.http.get("http://192.168.0.31/rpiapi/activate/7")
       .subscribe(response => response)
     }
-    pinOff24() {
-      let headers = new HttpHeaders();
-      headers.append('Content-Type', 'application/x-www-form-urlencoded');
-      headers.append('Accept', 'application/json');
-      headers.append('Authorization', 'Basic YWRtaW46RnJhZ2dsZVN0aWNrQ2Fy');
-
-      this.http.get("http://192.168.0.31/rpiapi/deactivate/24", {headers:headers})
+    pinOff7() {
+      this.http.get("http://192.168.0.31/rpiapi/deactivate/7")
       .subscribe(response => response)
+    }
+
+    //  Fans section
+    fanLow() {
+      this.http.get("http://192.168.0.31/rpiapi/deactivate/15")
+      .subscribe(response => {
+        if(response === 0) {
+          this.http.get("http://192.168.0.31/rpiapi/deactivate/13")
+          .subscribe(response => {
+            if(response === 0) {
+              this.http.get("http://192.168.0.31/rpiapi/activate/11")
+              .subscribe(response => console.log('response pin 11 low', response));
+            }
+          });
+        }
+      });
+    }
+   
+    fanMedium() {
+      this.http.get("http://192.168.0.31/rpiapi/deactivate/15")
+      .subscribe(response => {
+        if(response === 0) {
+          this.http.get("http://192.168.0.31/rpiapi/activate/11")
+          .subscribe(response => console.log('response pin 11 medium', response));
+          this.http.get("http://192.168.0.31/rpiapi/activate/13")
+          .subscribe(response => console.log('response pin 13 medium', response));
+        }
+      });
+    }
+    
+    fanHigh() {
+      this.http.get("http://192.168.0.31/rpiapi/deactivate/11")
+      .subscribe(response => {
+        if(response === 0) {
+          this.http.get("http://192.168.0.31/rpiapi/deactivate/13")
+          .subscribe(response => {
+            if(response === 0) {
+              this.http.get("http://192.168.0.31/rpiapi/activate/15")
+              .subscribe(response => console.log('response pin 15 high', response));
+            }
+          });
+        }
+      });
+    }
+
+    allFanPinsOff() {
+      this.http.get("http://192.168.0.31/rpiapi/deactivate/15")
+      .subscribe(response => console.log('response pin 15 fan off', response))
+      this.http.get("http://192.168.0.31/rpiapi/deactivate/13")
+      .subscribe(response => console.log('response pin 13 fan off', response))
+      this.http.get("http://192.168.0.31/rpiapi/deactivate/11")
+      .subscribe(response => console.log('response pin 11 fan off', response))
     }
 
 }
